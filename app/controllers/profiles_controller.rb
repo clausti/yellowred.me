a5b269dc1ceb52693d028db5e8d4bb88f77621ad
clausti <clausti@gmail.com>
2013-11-05 16:01:02 -0800

    add height to left sidebar, add heart-me label to profile edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,13 +3,14 @@ class ProfilesController < ApplicationController
   def show
     if params[:username]
       @profile = Profile.find_by_username(params[:username])
+      render :json => false, :status => 422 unless @profile
     else
       @profile = current_user.profile
     end
     render :json => @profile, :status => 200
   end
   
-  def edit #will only be hit on intial profile edit after signup
+  def edit #will _only_ be hit on intial profile edit after signup
     @profile = current_user.profile
     render :edit
   end
